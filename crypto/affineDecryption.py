076829cfe8da8abdfa4f40fe9a9f7280f6db69c7
ramibakir <rami.bakir@hotmail.no>
2022-11-01 12:48:35 +0100

    more obvious variable name

--- crypto/affineDecryption.py
+++ crypto/affineDecryption.py
@@ -6,22 +6,23 @@ au = alphabet.upper()
 
 
 def decrypt(string, a, b):
-    pt = ''
+    plaintext = ''
     ai = inverse(a, len(alphabet))
     for char in string:
         if char in alphabet:
-            pt += alphabet[(ai*(alphabet.index(char) - b)) % len(alphabet)]
+            plaintext += alphabet[(ai*(alphabet.index(char) - b)) %
+                                  len(alphabet)]
         elif char in au:
-            pt += au[(ai*(au.index(char) - b)) % len(au)]
+            plaintext += au[(ai*(au.index(char) - b)) % len(au)]
         else:
-            pt += char
-    return pt
+            plaintext += char
+    return plaintext
 
 
 cipherText = 'SQFPNB{Uznnz xfd zm bqm pqwlzd, hzm qææz ia iqææzd...}'
 
 for a in range(0, 10):
     for b in range(0, 10):
-        pt = decrypt(cipherText, a, b)
-        if pt.startswith('UIACTF'):
-            print(pt)
+        plaintext = decrypt(cipherText, a, b)
+        if plaintext.startswith('UIACTF'):
+            print(plaintext)
